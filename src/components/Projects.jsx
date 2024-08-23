f156462953c09caae7be9f7f94ff5a7fa194e3c5
Muhamad Fazril Maulana <mfazrilmaulana@gmail.com>
2024-08-23 20:17:53 +0700

    perubahan urutan project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -90,6 +90,43 @@ export default function Projects() {
         <div className="bg-[#06051A] max-w-80 sm:max-w-96 md:max-w-[400px] rounded-md hover:-translate-y-2 transition shadow-xl">
           <div className="rounded-md">
             <div className="rounded-md max-w-80 sm:max-w-96 md:max-w-[400px] overflow-hidden">
+              <img src="img/project6.png" alt="" className="object-cover" />
+            </div>
+          </div>
+          <p className="text-white text-sm sm:text-base p-2 mt-1">
+            a web application such as e-commerce with a culinary theme with
+            management features
+          </p>
+          <div className="flex gap-2 mb-2 ml-2">
+            <div className="w-10 sm:w-12 p-1 flex items-center">
+              <img src="img/html.png" alt="html" className="object-cover" />
+            </div>
+            <div className="w-10 sm:w-12 p-1 flex items-center">
+              <img
+                src="img/bootstrap.png"
+                alt="html"
+                className="object-cover"
+              />
+            </div>
+            <div className="w-10 sm:w-12 p-1 flex items-center">
+              <img
+                src="img/codeignitter.png"
+                alt="html"
+                className="object-cover"
+              />
+            </div>
+            <a
+              href="#"
+              className="bg-blue-700 text-center flex justify-center items-center px-2 rounded-md ml-9 hover:border-blue-700 hover:border-2 transition hover:bg-[#06051A]"
+            >
+              <p>See repo</p>
+            </a>
+          </div>
+        </div>
+        {/* card project 4*/}
+        <div className="bg-[#06051A] max-w-80 sm:max-w-96 md:max-w-[400px] rounded-md hover:-translate-y-2 transition shadow-xl">
+          <div className="rounded-md">
+            <div className="rounded-md max-w-80 sm:max-w-96 md:max-w-[400px] overflow-hidden">
               <img src="img/project3.png" alt="" className="object-cover" />
             </div>
           </div>
@@ -119,7 +156,7 @@ export default function Projects() {
             </a>
           </div>
         </div>
-        {/* card project 4 */}
+        {/* card project 5 */}
         <div className="bg-[#06051A] max-w-80 sm:max-w-96 md:max-w-[400px] rounded-md hover:-translate-y-2 transition shadow-xl">
           <div className="rounded-md">
             <div className="rounded-md max-w-80 sm:max-w-96 md:max-w-[400px] overflow-hidden">
@@ -156,7 +193,7 @@ export default function Projects() {
             </a>
           </div>
         </div>
-        {/* card project 5 */}
+        {/* card project 6 */}
         <div className="bg-[#06051A] max-w-80 sm:max-w-96 md:max-w-[400px] rounded-md hover:-translate-y-2 transition shadow-xl">
           <div className="rounded-md">
             <div className="rounded-md max-w-80 sm:max-w-96 md:max-w-[400px] overflow-hidden">
@@ -179,51 +216,14 @@ export default function Projects() {
             </div>
             <div className="w-10 sm:w-12 p-1 flex items-center">
               <img
-                src="img/codeignitter.png"
-                alt="html"
+                src="img/laravel.png"
+                alt="laravel"
                 className="object-cover"
               />
             </div>
             <a
               href="https://github.com/MhmdFazril/To-do-list-app"
               target="_blank"
-              className="bg-blue-700 text-center flex justify-center items-center px-2 rounded-md ml-9 hover:border-blue-700 hover:border-2 transition hover:bg-[#06051A]"
-            >
-              <p>See repo</p>
-            </a>
-          </div>
-        </div>
-        {/* card project 6*/}
-        <div className="bg-[#06051A] max-w-80 sm:max-w-96 md:max-w-[400px] rounded-md hover:-translate-y-2 transition shadow-xl">
-          <div className="rounded-md">
-            <div className="rounded-md max-w-80 sm:max-w-96 md:max-w-[400px] overflow-hidden">
-              <img src="img/project6.png" alt="" className="object-cover" />
-            </div>
-          </div>
-          <p className="text-white text-sm sm:text-base p-2 mt-1">
-            a web application such as e-commerce with a culinary theme with
-            management features
-          </p>
-          <div className="flex gap-2 mb-2 ml-2">
-            <div className="w-10 sm:w-12 p-1 flex items-center">
-              <img src="img/html.png" alt="html" className="object-cover" />
-            </div>
-            <div className="w-10 sm:w-12 p-1 flex items-center">
-              <img
-                src="img/bootstrap.png"
-                alt="html"
-                className="object-cover"
-              />
-            </div>
-            <div className="w-10 sm:w-12 p-1 flex items-center">
-              <img
-                src="img/codeignitter.png"
-                alt="html"
-                className="object-cover"
-              />
-            </div>
-            <a
-              href="#"
               className="bg-blue-700 text-center flex justify-center items-center px-2 rounded-md ml-9 hover:border-blue-700 hover:border-2 transition hover:bg-[#06051A]"
             >
               <p>See repo</p>
